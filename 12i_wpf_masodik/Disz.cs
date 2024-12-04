a51f739546686aed4c8e4fbfcbc25c51480a03e7
zsar.daniel <zsar.daniel@C19-TANAR>
2024-12-04 11:34:28 +0100

    Add project files.

---
+++ 12i_wpf_masodik/Disz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12i_wpf_masodik
+{
+    class Disz
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
+    }
+}
---
+++ 12i_wpf_masodik/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _12i_wpf_masodik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void CreateStoreItem(Disz oneDisz) {
+            Border oneBorder = new Border();
+            Store.Children.Add(oneBorder);
+            Grid oneGrid = new Grid();
+            oneBorder.Child = oneGrid;
+            RowDefinition firstRow = new RowDefinition();
+            RowDefinition secondRow = new RowDefinition();
+            RowDefinition thirdRow = new RowDefinition();
+            RowDefinition fourthRow = new RowDefinition();
+            oneGrid.RowDefinitions.Add(firstRow);
+            oneGrid.RowDefinitions.Add(secondRow);
+            oneGrid.RowDefinitions.Add(thirdRow);
+            oneGrid.RowDefinitions.Add(fourthRow);
+            TextBlock name = new TextBlock();
+            TextBlock price = new TextBlock();
+            TextBlock count = new TextBlock();
+            Button buyButton = new Button();
+            oneGrid.Children.Add(name);
+            oneGrid.Children.Add(price);
+            oneGrid.Children.Add(count);
+            oneGrid.Children.Add(buyButton);
+
+
+
+
+
+
+
+        }
+
+    }
+}
